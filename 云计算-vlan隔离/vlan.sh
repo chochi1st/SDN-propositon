cf7a27540b4815aa8ddb0de32a198e14cb88155a
chochi <chochi1st@gmail.com>
2018-06-30 12:08:55 +0800

    Update vlan.sh

--- 云计算-vlan隔离/vlan.sh
+++ 云计算-vlan隔离/vlan.sh
@@ -13,10 +13,10 @@ sudo ovs-ofctl add-flow s1 -O OpenFlow13 "table=1,priority=50,dl_vlan=20,actions
 sudo ovs-ofctl add-flow s1 -O OpenFlow13 "table=1,priority=50,dl_vlan=30,actions=output:patch12" # h3->h4 vlan:30 : output (chochi)
 sudo ovs-ofctl dump-flows -O OpenFlow13 s1 table=1
 ## s1 - table 2
-sudo ovs-ofctl add-flow s1 -O OpenFlow13 "table=2,priority=50,dl_vlan=20,actions=strip_vlan,output:1" # h2->h1 : output=2(h1)
+sudo ovs-ofctl add-flow s1 -O OpenFlow13 "table=2,priority=50,dl_vlan=20,actions=strip_vlan,output:1" # h2->h1 : output=(h1)
 sudo ovs-ofctl dump-flows -O OpenFlow13 s1 table=2
 ## s1 - table 3
-sudo ovs-ofctl add-flow s1 -O OpenFlow13 "table=3,priority=50,dl_vlan=30,actions=strip_vlan,output:2" # h4->h2 : output=3(h3)
+sudo ovs-ofctl add-flow s1 -O OpenFlow13 "table=3,priority=50,dl_vlan=30,actions=strip_vlan,output:2" # h4->h2 : output=(h3)
 sudo ovs-ofctl dump-flows -O OpenFlow13 s1 table=3
 ## s1 - table 4
 sudo ovs-ofctl add-flow s1 -O OpenFlow13 "table=4,priority=50,actions=1,2"                            #外网数据输入
@@ -36,11 +36,11 @@ sudo ovs-ofctl add-flow s2 -O OpenFlow13 "table=1,priority=50,dl_vlan=30,actions
 sudo ovs-ofctl dump-flows -O OpenFlow13 s2 table=1
 
 # s3 - table 2
-sudo ovs-ofctl add-flow s2 -O OpenFlow13 "table=2,priority=50,dl_vlan=20,actions=strip_vlan,output:1"         # h1->h2 : output=2(h2)
+sudo ovs-ofctl add-flow s2 -O OpenFlow13 "table=2,priority=50,dl_vlan=20,actions=strip_vlan,output:1"         # h1->h2 : output=(h2)
 sudo ovs-ofctl dump-flows -O OpenFlow13 s2 table=2
 
 # s3 -table 3
-sudo ovs-ofctl add-flow s2 -O OpenFlow13 "table=3,priority=50,dl_vlan=30,actions=strip_vlan,output:2"         # h3->h4 : output=3(h4)
+sudo ovs-ofctl add-flow s2 -O OpenFlow13 "table=3,priority=50,dl_vlan=30,actions=strip_vlan,output:2"         # h3->h4 : output=(h4)
 sudo ovs-ofctl dump-flows -O OpenFlow13 s2 table=3
 # s3 - table 4
 sudo ovs-ofctl add-flow s2 -O OpenFlow13 "table=4,priority=50,actions=1,2"                                    # 外网数据输入 
